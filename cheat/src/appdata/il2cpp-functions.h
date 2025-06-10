a4dabc46911255b7b7bab6229abdbe8caca33acd
Zayron <45888349+zayronnn@users.noreply.github.com>
2025-06-09 21:58:19 -0300

    update version 1.2.67 novo

--- cheat/src/appdata/il2cpp-functions.h
+++ cheat/src/appdata/il2cpp-functions.h
@@ -6,40 +6,42 @@
 // ******************************************************************************
 using namespace app;
 
-DO_APP_FUNC(0x0096D2F0, void, GameFrameWork_Update, (void/*GameFrameWork*/* __this, MethodInfo* method));
+DO_APP_FUNC(0x0096E2F0, void, GameFrameWork_Update, (void/*GameFrameWork*/* __this, MethodInfo* method));
 
-DO_APP_FUNC(0x037F0C50, String*, Marshal_PtrToStringAnsi, (void* ptr, MethodInfo* method));
-DO_APP_FUNC(0x037F0C70, String*, Marshal_PtrToStringUni, (void* ptr, MethodInfo* method));
+DO_APP_FUNC(0x037F0A70, String*, Marshal_PtrToStringAnsi, (void* ptr, MethodInfo* method));
+DO_APP_FUNC(0x037F0A90, String*, Marshal_PtrToStringUni, (void* ptr, MethodInfo* method));
 
 // FovChanger
-DO_APP_FUNC(0x049489C0, void, Camera_set_fieldOfView, (void* __this, float value, MethodInfo* method));
+DO_APP_FUNC(0x049487E0, void, Camera_set_fieldOfView, (void* __this, float value, MethodInfo* method));
 
 // C# methods
-DO_APP_FUNC(0x0392E5E0, Type*, Type_GetType_3, (String* typeName, MethodInfo* method));
-DO_APP_FUNC(0x03804DE0, Type*, Assembly_GetType, (Assembly* __this, String* name, MethodInfo* method));
-DO_APP_FUNC(0x03805300, Assembly*, Assembly_LoadWithPartialName, (String* partialName, MethodInfo* method));
+DO_APP_FUNC(0x0392E400, Type*, Type_GetType_3, (String* typeName, MethodInfo* method));
+DO_APP_FUNC(0x03804C00, Type*, Assembly_GetType, (Assembly* __this, String* name, MethodInfo* method));
+DO_APP_FUNC(0x03805120, Assembly*, Assembly_LoadWithPartialName, (String* partialName, MethodInfo* method));
 
 
 // Unity methods
-DO_APP_FUNC(0x0498D240, Transform*, GameObject_get_transform, (GameObject* __this, MethodInfo* method));
-DO_APP_FUNC(0x0498C1F0, GameObject*, GameObject_Find, (String* name, MethodInfo* method));
-DO_APP_FUNC(0x04991E90, String*, Object_1_get_name, (Object_1* __this, MethodInfo* method));
-DO_APP_FUNC(0x0499DD30, int32_t, Transform_get_childCount, (Transform* __this, MethodInfo* method));
-DO_APP_FUNC(0x049B86A0, String*, Scene_GetNameInternal, (int32_t sceneHandle, MethodInfo* method));
-DO_APP_FUNC(0x049B7070, Scene, SceneManager_GetActiveScene, (MethodInfo* method));
+DO_APP_FUNC(0x0498D060, Transform*, GameObject_get_transform, (GameObject* __this, MethodInfo* method));
+DO_APP_FUNC(0x0498C010, GameObject*, GameObject_Find, (String* name, MethodInfo* method));
+DO_APP_FUNC(0x04991CB0, String*, Object_1_get_name, (Object_1* __this, MethodInfo* method));
+DO_APP_FUNC(0x0499DB50, int32_t, Transform_get_childCount, (Transform* __this, MethodInfo* method));
+DO_APP_FUNC(0x049B84C0, String*, Scene_GetNameInternal, (int32_t sceneHandle, MethodInfo* method));
+DO_APP_FUNC(0x049B6E90, Scene, SceneManager_GetActiveScene, (MethodInfo* method));
 
 // No Cooldown
-DO_APP_FUNC(0x011B2EE0, void/*KAAIFMKPKAG*/*, KAAIFMKPKAG_IOFMGMJCCFO, (KAAIFMKPKAG* __this, bool DEJNILEHENL, MethodInfo* method)); //Poto de referencia 23 para cima encontre o System Collection generic 
+DO_APP_FUNC(0x011C0E30, void/*KAAIFMKPKAG*/*, KAAIFMKPKAG_IOFMGMJCCFO, (KAAIFMKPKAG* __this, bool DEJNILEHENL, MethodInfo* method)); //"Signature": "Cinemachine_CinemachineVirtualCameraBase_o*  ache esse código único,
+//e diminua por -204 e vai achar o valor correto
+
 // Damage Hack
-DO_APP_FUNC(0x0117C8C0, void, PIPHNBOBFEF_KBCIIEFLPGB, (PIPHNBOBFEF* __this, ESpecialState__Enum OCKAHFDGNEL, int64_t MGFPJFANLBG, int64_t DIACKALMMKH, int64_t PBAPBJGDAPJ, String* DLOACPLHGMP, MethodInfo* method));
+DO_APP_FUNC(0x011873A0, void, PIPHNBOBFEF_KBCIIEFLPGB, (PIPHNBOBFEF* __this, ESpecialState__Enum OCKAHFDGNEL, int64_t MGFPJFANLBG, int64_t DIACKALMMKH, int64_t PBAPBJGDAPJ, String* DLOACPLHGMP, MethodInfo* method));
 // God Mode
-DO_APP_FUNC(0x016DA150, int32_t, GHINOEFFMPN_EKHGIHBHEPL, (SkillIdentity* EKPKPIJBPKE, void/*IEnumerable_1_UnityEngine_Component_*/* FKJDKGJBGOD, TargetHitData* COFCDKGCOGJ, MethodInfo* method));// 7 para cima * __this, UnityEngine_Component_array**
+DO_APP_FUNC(0x01744C80, int32_t, GHINOEFFMPN_EKHGIHBHEPL, (SkillIdentity* EKPKPIJBPKE, void/*IEnumerable_1_UnityEngine_Component_*/* FKJDKGJBGOD, TargetHitData* COFCDKGCOGJ, MethodInfo* method));// 7 para baixo * __this, UnityEngine_Component_array**
 
 // Dumb Enemies
-DO_APP_FUNC(0x017E1190, void, EvadeProxy_Init, (EvadeProxy* __this, ENNEJEPMJLJ* IGBKKNODEGM, EvadePenetration* DHPMEMDBDMC, MethodInfo* method));
+DO_APP_FUNC(0x017E2390, void, EvadeProxy_Init, (EvadeProxy* __this, ENNEJEPMJLJ* IGBKKNODEGM, EvadePenetration* DHPMEMDBDMC, MethodInfo* method));
 
 
 // Shadow Cooldown And Limit
-DO_APP_FUNC(0x017BFDF0, void, JLLMICINIDA_LFCGJLAFFOM, (JLLMICINIDA* __this, MethodInfo* method)); //15 para baixo "Signature": "void NGame_TeamFightWorldInfo___ctor (NGame_TeamFightWorldInfo_o* __this, const MethodInfo* method);",
+DO_APP_FUNC(0x017C1E70, void, JLLMICINIDA_LFCGJLAFFOM, (JLLMICINIDA* __this, MethodInfo* method)); //15 para cima "Signature": "void NGame_TeamFightWorldInfo___ctor (NGame_TeamFightWorldInfo_o* __this, const MethodInfo* method);",
 
-DO_APP_FUNC(0x00576CD0, Object*, TSingleton_1_System_Object__get_Instance, (MethodInfo* method)); // "Name": "System.Buffers.ArrayPool\u003Cint\u003E$$get_Shared",
+DO_APP_FUNC(0x00577CD0, Object*, TSingleton_1_System_Object__get_Instance, (MethodInfo* method)); // "Name": "System.Buffers.ArrayPool\u003Cint\u003E$$get_Shared",
